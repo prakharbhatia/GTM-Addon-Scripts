b38f5fc2df2435a684317c4db638b8e01e9baba6
prakharbhatia <prakharb88@gmail.com>
2022-07-06 09:43:33 +0530

    1

--- gtm-addon-scripts.php
+++ gtm-addon-scripts.php
@@ -76,7 +76,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-gtm-addon-scripts.php';
 
 require plugin_dir_path( __FILE__ ) . 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://raw.githubusercontent.com/prakharbhatia/GTM-Addon-Scripts/419c170ff09bce88f90cf0ea1e568a421bfe020d/plugin-update-checker/examples/plugin.json',
+	'https://raw.githubusercontent.com/prakharbhatia/GTM-Addon-Scripts/main/plugin-update-checker/examples/plugin.json',
 	__FILE__, //Full path to the main plugin file or functions.php.
 	'gtm-addon-scripts'
 );
